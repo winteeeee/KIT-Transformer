121579ed25fdf99c08bbed3a21f6bbd5856e3691
wintee-desktop <b12e34e56@naver.com>
2024-01-09 20:40:02 +0900

    [Embedding :: docstring 추가]

--- src/modules/embedding.py
+++ src/modules/embedding.py
@@ -4,6 +4,15 @@ from positional_encoder import positional_encode
 
 
 def embedding(inputs, vocab_size, d_model, dropout):
+    """
+    keras의 Embedding층을 이용해 입력을 임베딩하고 가중치를 √d_model와 곱한 후 포지셔널 인코딩 행렬과 더한, 최종적으로 트랜스포머가 사용할 임베딩 행렬을 반환
+
+    :param inputs: 모델의 입력
+    :param vocab_size: 입력에서의 총 vocab 크기
+    :param d_model: 모델의 하이퍼파라미터
+    :param dropout: 모델의 하이퍼파라미터
+    :return: 입력을 임베딩한 행렬
+    """
     embedding_vector = Embedding(vocab_size, d_model)(inputs)
     embedding_vector *= tf.math.sqrt(tf.cast(d_model, tf.float32))
     embedding_vector += positional_encode(vocab_size, d_model)
