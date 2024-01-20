368e7bca19574c8dd10298ab555879cc05e0cd75
wintee-desktop <b12e34e56@naver.com>
2024-01-20 19:33:55 +0900

    [Stacking Encoders]

--- src/modules/embedding.py
+++ src/modules/embedding.py
@@ -1,20 +1,17 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Embedding, Dropout
-from positional_encoder import positional_encode
+from tensorflow.keras.layers import Embedding
 
 
-def embedding(inputs, vocab_size, d_model, dropout):
+def embedding(inputs, vocab_size, d_model):
     """
-    keras의 Embedding층을 이용해 입력을 임베딩하고 가중치를 √d_model와 곱한 후 포지셔널 인코딩 행렬과 더한, 최종적으로 트랜스포머가 사용할 임베딩 행렬을 반환
+    keras의 Embedding층을 이용해 입력을 임베딩하고 가중치를 √d_model와 곱한 임베딩 행렬을 반환
 
     :param inputs: 모델의 입력
     :param vocab_size: 입력에서의 총 vocab 크기
     :param d_model: 모델의 하이퍼파라미터
-    :param dropout: 모델의 하이퍼파라미터
     :return: 입력을 임베딩한 행렬
     """
     embedding_vector = Embedding(vocab_size, d_model)(inputs)
     embedding_vector *= tf.math.sqrt(tf.cast(d_model, tf.float32))
-    embedding_vector += positional_encode(vocab_size, d_model)
-    # 기본적으로 드롭아웃은 서브층 이후에 적용되지만 예외적으로 임베딩 벡터 + 포지셔널 인코딩 작업 이후에도 적용
-    return Dropout(rate=dropout)(embedding_vector)
+
+    return embedding_vector
--- src/transformer.py
+++ src/transformer.py
@@ -1,16 +1,23 @@
 from modules.encoder import Encoder
+from modules.embedding import embedding
+from modules.positional_encoder import positional_encoding
 from decoder import Decoder
 
+import tensorflow as tf
 
-class Transformer:
-    def __init__(self, d_model=512, num_layers=6, num_heads=8, d_ff=2048, dropout=0.1):
+
+class Transformer(tf.keras.Model):
+    def __init__(self, vocab_size, d_model=512, num_layers=6, num_heads=8, d_ff=2048, dropout=0.1):
         """
+        :param vocab_size: 학습 데이터의 단어 크기
         :param d_model: 인코더와 디코더의 입출력 크기 및 임베딩 차원을 설정. 기본값 512
         :param num_layers: 인코더와 디코더의 층 수. 기본값 6
         :param num_heads: 병렬 어텐션 개수. 기본값 8
         :param dropout: Dropout층의 Rate
         :param d_ff: 내부 피드 포워드 신경망의 은닉층 크기. 기본값 2048
         """
+        super(Transformer, self).__init__()
+        self.vocab_size = vocab_size
         self.d_model = d_model
         self.num_layers = num_layers
         self.num_heads = num_heads
@@ -19,3 +26,13 @@ class Transformer:
 
         self.encoder = Encoder(d_model=d_model, num_heads=num_heads, dropout=dropout, d_ff=d_ff)
         self.decoder = Decoder()
+
+    def call(self, inputs):
+        inputs = embedding(inputs=inputs, vocab_size=self.vocab_size, d_model=self.d_model)
+        inputs = positional_encoding(inputs=inputs,
+                                     vocab_size=self.vocab_size,
+                                     d_model=self.d_model,
+                                     dropout=self.dropout)
+
+        for i in range(self.num_layers):
+            inputs = self.encoder.encode(inputs)
